bbfa2de277e400deca3cc6c442ae32d33be42857
TheoRevFdz <hrevillafernandez79@gmail.com>
2022-10-31 11:00:13 -0500

    Move implements to package impl

--- src/main/java/com/nttdata/bootcamp/mscustomers/interfaces/impl/ProfileServiceImpl.java
+++ src/main/java/com/nttdata/bootcamp/mscustomers/interfaces/impl/ProfileServiceImpl.java
@@ -1,4 +1,4 @@
-package com.nttdata.bootcamp.mscustomers.interfaces;
+package com.nttdata.bootcamp.mscustomers.interfaces.impl;
 
 import java.util.List;
 import java.util.Optional;
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nttdata.bootcamp.mscustomers.infraestructure.IProfileRepository;
+import com.nttdata.bootcamp.mscustomers.interfaces.IProfileService;
 import com.nttdata.bootcamp.mscustomers.model.Profile;
 
 @Service
